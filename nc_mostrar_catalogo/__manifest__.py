ecaeef8ce875bfb9fd597372a056d3311347e4a3
alfconta <norn78796@gmail.com>
2020-12-21 14:56:51 -0600

    Updated

---
+++ nc_mostrar_catalogo/__manifest__.py
@@ -0,0 +1,18 @@
+# Author: NICA-CREATOR
+# Copyright 2019 
+
+{
+    'name': 'show chart account',
+    'version': '13.0.1.1.1',
+    'support':'gtnorw@yahoo.com',   
+    'category': 'Reporting',
+    'summary': 'show chart account',   
+    'author': 'NICA-CREATOR', 
+    "depends": ["base", "account"],
+    'images': ['static/description/main_screenshot.png'],
+    'data': ['mostrar_catalogo.xml',],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'license': 'OPL-1',
+}
---
+++ nc_mostrar_catalogo/mostrar_catalogo.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+import odoo.addons.decimal_precision as dp
+
+
